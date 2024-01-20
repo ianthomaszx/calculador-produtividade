f3853eccd0285948655350b991b3697d4f5bffb7
ianthomaszx <ianthomaszx@gmail.com>
2024-01-20 16:10:55 -0400

    pizza 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-document.getElementById('calculate').addEventListener('click', function(){
+document.getElementById('calculate').addEventListener('click', function () {
     const os = document.getElementById('os').value * 0.960714;
     const apt = (document.getElementById('apt').value) * 0.116923;
     const itens = document.getElementById('itens').value * 0.01375;
